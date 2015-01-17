e7e9c57d8d2d705b4fa74c747cb0f96716205608
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-01-17 13:12:19 -0800

    add course text box and an add button.  the add butfon takes to a new activity

--- app/src/main/java/com/example/marculator/comp3717/addActivity.java
+++ app/src/main/java/com/example/marculator/comp3717/addActivity.java
@@ -1,29 +1,24 @@
 package com.example.marculator.comp3717;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 
-public class addCourseScreen extends ActionBarActivity {
-
-    public final static String EXTRA_MESSAGE = "com.calculatorAssign.myapplication.addActivity";
+public class addActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_course_screen);
+        setContentView(R.layout.add_activity);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_course_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_add, menu);
         return true;
     }
 
@@ -41,15 +36,4 @@ public class addCourseScreen extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    //Called when the user clicks the add button for add course screen
-    public void sendMessage(View view){
-        Intent intent = new Intent(this,addActivity.class);
-        EditText editText = (EditText)findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-
 }
