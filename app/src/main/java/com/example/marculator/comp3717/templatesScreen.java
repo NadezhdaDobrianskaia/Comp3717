69f6f2a9b1107086d332a436e0f8fdb055e78620
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-02-11 00:52:06 -0800

    templateScreen Java set up to take in different edit buttons with different course names, warning line 77 - the else statement has empty body

--- app/src/main/java/com/example/marculator/comp3717/templatesScreen.java
+++ app/src/main/java/com/example/marculator/comp3717/templatesScreen.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -16,6 +17,7 @@ public class templatesScreen extends ActionBarActivity {
 
 //    public final static String EXTRA_MESSAGE = "com.calculatorAssign.myapplication.editCourseDetails";
     TextView course1, course2, course3, course4;
+    Button editCourse1, editCourse2, editCourse3, editCourse4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,10 @@ public class templatesScreen extends ActionBarActivity {
         course2 = (TextView)findViewById(R.id.textView_course2);
         course3 = (TextView)findViewById(R.id.textView_course3);
         course4 = (TextView)findViewById(R.id.textView_course4);
+        editCourse1 = (Button)findViewById(R.id.templates_editBtn_course1);
+        editCourse2 = (Button)findViewById(R.id.templates_editBtn_course2);
+        editCourse3 = (Button)findViewById(R.id.templates_editBtn_course3);
+        editCourse4 = (Button)findViewById(R.id.templates_editBtn_course4);
     }
 
 
@@ -58,6 +64,22 @@ public class templatesScreen extends ActionBarActivity {
     //Called when the user clicks the add button for add course screen
     public void editCourseDetails(View view){
         Intent i = new Intent(this,editCourseDetails.class);
+        Bundle course = new Bundle();
+        //add if statment to make sure that the edit is editing the right one
+        if(view == course1)
+            course.putString("course",course1.getText().toString());
+        else if(view == course2)
+            course.putString("course",course2.getText().toString());
+        else if(view == course3)
+            course.putString("course",course3.getText().toString());
+        else if(view == course4)
+            course.putString("course",course4.getText().toString());
+        else //make an error
+            ;
+        i.putExtras(course);
+
+
+
         startActivityForResult(i, 2);
     }
 
