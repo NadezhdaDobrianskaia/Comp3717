750fd92f754d34b853f93959a468c0d04f66f5d5
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-03-11 11:51:37 -0700

    Adding Course class and Item class

---
+++ app/src/main/java/com/example/marculator/comp3717/Course.java
@@ -0,0 +1,71 @@
+package com.example.marculator.comp3717;
+
+import android.widget.Button;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Course implements Serializable {
+    private String courseName = "";
+    private String category = "";
+    private Button editButton = null;
+    private ArrayList Items = null;
+
+    //constructor called when add button is pressed without new Button
+    public Course(String courseName) {
+        this.courseName = courseName;
+        this.category = null;
+        this.Items = null;
+
+    }
+    //constructor without the new button
+    public Course(String courseName, String category) {
+        this.courseName = courseName;
+        this.category = category;
+
+    }
+
+    //constructor with a new button
+    public Course(String courseName, String category, Button editButton) {
+        this.courseName = courseName;
+        this.category = category;
+        this.editButton = editButton;
+    }
+
+
+    public Button getEditButton() {
+        return editButton;
+    }
+
+    public void setEditButton(Button editButton) {
+        this.editButton = editButton;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public ArrayList getItems() {
+        return Items;
+    }
+
+    public void setItems(ArrayList items) {
+        Items = items;
+    }
+
+    public void addItems(Item i){
+        Items.add(i);
+    }
+}
---
+++ app/src/main/java/com/example/marculator/comp3717/Item.java
@@ -0,0 +1,46 @@
+package com.example.marculator.comp3717;
+
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    private String itemName = "";
+    private String category = "";
+    private double weight;
+
+    //constructor
+    public Item(String itemName, String category, double weight){
+        this.itemName = itemName;
+        this.category = category;
+        this.weight = weight;
+    }
+    public Item(String itemName, double weight){
+        this.itemName = itemName;
+        this.category = null;
+        this.weight = weight;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
