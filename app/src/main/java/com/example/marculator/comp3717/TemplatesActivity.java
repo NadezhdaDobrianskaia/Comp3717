40fdd4bdfe67104f4140afb2971585f9bf44e2ee
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-03-14 09:00:15 -0700

    Saving to a file.  file is saved when the save button is clicked from the templates activity.  when the app loads again the data is loaded into the phone

--- app/src/main/java/com/example/marculator/comp3717/TemplatesActivity.java
+++ app/src/main/java/com/example/marculator/comp3717/TemplatesActivity.java
@@ -1,6 +1,7 @@
 package com.example.marculator.comp3717;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,6 +12,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 
@@ -27,7 +36,7 @@ public class TemplatesActivity extends ListActivity {
         ListView listView = getListView();
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         listView.setTextFilterEnabled(true);
-
+        loadData();
     }
 
 
@@ -68,6 +77,50 @@ public class TemplatesActivity extends ListActivity {
 
         courseDetails.putExtra("myCourse", myCourse);
         startActivityForResult(courseDetails, 2);
+
+    }
+
+    public void save_data(View v){
+        try{
+            FileOutputStream fOut = openFileOutput("dataList.bin",MODE_PRIVATE);
+            ObjectOutputStream osw = new ObjectOutputStream(fOut);
+            osw.writeObject(courseList);
+            osw.flush();
+            osw.close();
+            Toast.makeText(getBaseContext(), "file Saved successfully", Toast.LENGTH_SHORT).show();
+        }
+        catch (FileNotFoundException e){
+            Toast.makeText(getBaseContext(), "FileNotfound", Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException ioe){
+            Toast.makeText(getBaseContext(), "io", Toast.LENGTH_SHORT).show();
+            ioe.printStackTrace();
+        }
+    }
+    public void loadData(){
+        try {
+            FileInputStream fIn = openFileInput("dataList.bin");
+            ObjectInputStream isr = new ObjectInputStream(fIn);
+
+            courseList = (ArrayList<Course>)isr.readObject();
+            isr.close();
+            fIn.close();
+            Toast.makeText(getBaseContext(),(courseList.get(0)).getCourseName(),Toast.LENGTH_LONG).show();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course temp = courseList.get(i);
+                display.add(temp.getCourseName());
+            }
+            setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, display));
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        catch(ClassNotFoundException cnf){
+            cnf.printStackTrace();
+        }
 
     }
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
